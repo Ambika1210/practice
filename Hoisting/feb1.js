7b6616f4a1d2b8d9e3d864a05bcbe57276045b20
Ambika Jha <ambika.ambika1210@gmail.com>
2026-02-02 15:23:41 +0530

    service

--- Hoisting/feb1.js
+++
@@ -1,8 +0,0 @@
-const createUser = async (userData) => {
-  try {
-    const user = new User(userData);
-    return await user.save();
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
